48c3eef8c02d1709d432e2d3973111493d69edb4
underleak <70888119+underleak@users.noreply.github.com>
2020-12-06 19:12:11 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "login.h"
 #include <QApplication>
+#include <stdlib.h>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
+    QFile file("style.qss");
+    file.open(QFile::ReadOnly);
+    application.setStyleSheet(file.readAll());
     Login login;
     login.show();
     return application.exec();
